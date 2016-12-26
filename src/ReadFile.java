15d3a770fbfc8b98790b733039058e3aec9c6006
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-26 17:23:32 +0100

    Initial commit

---
+++ src/ReadFile.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by Михаил on 26.12.2016.
+ */
+public class ReadFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        /*Open a file with the name hello.txt.*/
+        PrintWriter out = new PrintWriter("hello.txt");
+        /*Store the message “Hello, World!” in the file.*/
+        out.println("Hello, World!");
+        //Close the file.
+        out.close();
+        /*Open the same file again.*/
+        File inputFile = new File("hello.txt");
+        Scanner sc = new Scanner(inputFile);
+        while (sc.hasNext()) {
+            String text = sc.nextLine();
+            System.out.println(text);
+        }
+    }
+}
