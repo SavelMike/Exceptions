b5e12ecee3e9d20402340e3408f5018544207022
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-28 16:51:19 +0100

    P 7.5,  program works

---
+++ src/CountFile.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Михаил on 28.12.2016.
+ */
+public class CountFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Ask user for file name;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input file name: ");
+        String filename = in.nextLine();
+        //Open file by scanner with filename;
+        File f = new File(filename);
+        Scanner sc = new Scanner(f);
+        //read the file and count characters, words and lines
+        int numOfLines = 0;
+        int numOfwords = 0;
+        int numOfChar = 0;
+        while(sc.hasNextLine()) {
+            String s = sc.nextLine();
+            numOfLines++;
+            numOfChar += s.length();
+            Scanner strsc = new Scanner(s);
+            while (strsc.hasNext()) {
+                strsc.next();
+                numOfwords++;
+            }
+        }
+        System.out.println("Number of line: " + numOfLines + ", words: " + numOfwords + ", chars: " + numOfChar);
+    }
+}
+
