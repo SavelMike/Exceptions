0d5b95831768ef7b4dfdb9139bad4d85b5bf975f
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-28 20:21:00 +0100

    P 7.6

--- src/Find.java
+++ src/Find.java
@@ -1,10 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by Михаил on 28.12.2016.
  */
 public class Find {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length < 2) {
             System.out.println("Not enough arguments.");
             System.exit(1);
@@ -13,7 +15,14 @@ public class Find {
         String word = args[0];
         for (int i = 1; i < args.length; i++ ) {
             System.out.println(args[i]);
+            File input = new File(args[i]);
+            Scanner in = new Scanner(input);
+            while (in.hasNextLine()) {
+                String next = in.nextLine();
+                if (next.contains(word)) {
+                    System.out.println(input + ": " + next);
+                }
+            }
         }
-        
     }
 }
