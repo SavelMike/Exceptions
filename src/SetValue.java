0fd883de0faae0a5bf0fce3d8e23ab7eea1fca2f
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-30 22:23:07 +0100

    P 7.13

--- src/SetValue.java
+++ src/SetValue.java
@@ -15,7 +15,7 @@ public class SetValue {
                 D = sc.nextDouble();
                 return true;
             } catch (InputMismatchException exception) {
-
+                sc.next();
             }
         }
         return false;
@@ -29,3 +29,43 @@ public class SetValue {
         System.out.println(sum);
     }
 }
+
+import java.util.InputMismatchException;
+        import java.util.Scanner;
+
+/**
+ * Created by Михаил on 22.12.2016.
+ */
+    public class SetNum {
+        public double inputNum() throws InputMismatchException {
+            System.out.println("Enter the next value: ");
+            Scanner sc = new Scanner(System.in);
+            double num = sc.nextDouble();
+            return num;
+        }
+        public static void main(String[] args) {
+            double[] arr = new double[5];
+            int counter = 0;
+            int chance = 0;
+            try {
+                while (counter < arr.length) {
+                    new SetNum().inputNum();
+                    Scanner sc = new Scanner(System.in);
+                    counter++;
+                }
+            for (double arrs:arr){
+                System.out.println(arrs);
+            }
+        } catch (InputMismatchException ime) {
+            System.out.println("I am giving you one more chance");
+            try {
+                String[] str = {};
+                main(str);
+                new SetNum().inputNum();
+            } catch (InputMismatchException ime2) {
+                System.out.println("Now you loose");
+            }
+        }
+    }
+}
+
