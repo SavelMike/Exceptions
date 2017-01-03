a5cbd868a345d549d7e534d046baf5cb5744b9a4
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-03 15:35:24 +0100

    P 7.13 program works

--- src/SetValue.java
+++ src/SetValue.java
@@ -5,67 +5,40 @@ import java.util.InputMismatchException;
  * Created by Михаил on 30.12.2016.
  */
 public class SetValue {
-    public static double D;
 
-    public static boolean user_gave_double() {
+    // arguments: none
+// return value: boolean
+// description: let user enter double value twice.
+// If user entered double - set variable D and return true
+// if user did not enter double in two tries - return false
+// catch wrong input with exception (InputMismatchException)
+
+    public static double user_gave_double() {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 2; i++) {
             try {
                 System.out.print("Input data: ");
-                D = sc.nextDouble();
-                return true;
+                return sc.nextDouble();
             } catch (InputMismatchException exception) {
                 sc.next();
             }
         }
-        return false;
+        throw new NumberFormatException("");
     }
 
     public static void main(String[] args) {
         double sum = 0;
-        while (user_gave_double()) {
-            sum += D;
-        }
-        System.out.println(sum);
-    }
-}
-
-import java.util.InputMismatchException;
-        import java.util.Scanner;
-
-/**
- * Created by Михаил on 22.12.2016.
- */
-    public class SetNum {
-        public double inputNum() throws InputMismatchException {
-            System.out.println("Enter the next value: ");
-            Scanner sc = new Scanner(System.in);
-            double num = sc.nextDouble();
-            return num;
-        }
-        public static void main(String[] args) {
-            double[] arr = new double[5];
-            int counter = 0;
-            int chance = 0;
+        while (true) {
             try {
-                while (counter < arr.length) {
-                    new SetNum().inputNum();
-                    Scanner sc = new Scanner(System.in);
-                    counter++;
-                }
-            for (double arrs:arr){
-                System.out.println(arrs);
+                sum += user_gave_double();
             }
-        } catch (InputMismatchException ime) {
-            System.out.println("I am giving you one more chance");
-            try {
-                String[] str = {};
-                main(str);
-                new SetNum().inputNum();
-            } catch (InputMismatchException ime2) {
-                System.out.println("Now you loose");
+            catch(NumberFormatException ex)
+            {
+                System.out.println(sum);
+                break;
             }
         }
     }
 }
+
 
